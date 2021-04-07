606e363d82b77747e113a06421ad7e44bab061d0
dltmdwlr <dltmdwlr@gmail.com>
2021-04-07 10:48:44 +0900

    배열

--- 20210407/20210407/Program.cs
+++ 20210407/20210407/Program.cs
@@ -10,12 +10,12 @@ namespace _20210407
     {
         static void Main(string[] args)
         {
-            //1차원 배열 초기화
-            int[] arrlnt = { 10, 20, 30, 40 };
-            for (int i = 0; i < arrlnt.Length; i++)
+            //1차원 배열 초기화1
+            int[] arrInt = { 10, 20, 30, 40 };
+            for (int i = 0; i < arrInt.Length; i++)
             {
-                Console.Write(arrlnt[i] + " ");
-                string str = string.Format("배열[{0}]:{1}", i, arrlnt[i]);
+                Console.Write(arrInt[i] + " ");
+                string str = string.Format("배열[{0}]:{1}", i, arrInt[i]);
                 Console.WriteLine(str);
             }
 
@@ -27,6 +27,52 @@ namespace _20210407
             for (int i = 0; i < arrCh.Length; i++)
             {
                 Console.Write(arrCh[i] + " ");
+            }
+            Console.WriteLine();
+
+            //2차원 배열 초기화1
+            int[,] arrInt2 =
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 }
+            };
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(arrInt2[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            //2차원 배열 초기화2
+            const int SIZE_ROW = 2;
+            const int SIZE_COL = 3;
+            int[,] arrInt3 = new int[SIZE_ROW, SIZE_COL]
+            {
+                {10, 20, 30},
+                {40, 50, 60}
+            };
+            for (int i = 0; i < SIZE_ROW; i++)
+            {
+                for (int j = 0; j < SIZE_ROW; j++)
+                {
+                    Console.Write(arrInt2[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            //2차원 배열 초기화3
+            string[][] arrStr1 = new string[2][];
+            arrStr1[0] = new string[] { "홍길동", "김길동", "박길동" };
+            arrStr1[1] = new string[] { "전우치" };
+            for (int i = 0; i < arrStr1.Length; i++)
+            {
+                for (int j = 0; j < arrStr1[i].Length; j++)
+                {
+                    Console.Write(arrStr1[i][j] + " ");
+                }
+                Console.WriteLine();
             }
         }
     }
