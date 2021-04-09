438417ec16252e75a92ba969d45d5b200bba727e
dltmdwlr <dltmdwlr@gmail.com>
2021-04-09 11:54:54 +0900

    d

--- 20210409/Main/Program.cs
+++ 20210409/Main/Program.cs
@@ -1,4 +1,5 @@
 ﻿using _20210409.Model;
+using _20210409.View;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,12 +12,36 @@ namespace _20210409
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(new Car().ToString());
-            new Customer("고길동", 100, "010-1111-1111", "서울 턱별시", '남').CustomerInfo();
-            Console.WriteLine();
+            /*            Console.WriteLine(new Car().ToString());
+                        new Customer("고길동", 100, "010-1111-1111", "서울 턱별시", '남').CustomerInfo();
+                        Console.WriteLine();
 
-            Console.WriteLine("영업소 : " + Seller.CENTER);
-            new Seller("김준석", 28, "사장").sellerinfo();
+                        Console.WriteLine("영업소 : " + Seller.CENTER);
+                        new Seller("김준석", 28, "사장").sellerinfo();*/
+            
+            Menu menu = new Menu();
+
+            while (true)
+            {
+                switch (menu.mainMenu())
+                {
+                    case Menu.MENU_MAIN_RAND:
+                        break;
+                    case Menu.MENU_MAIN_DEL_ALL:
+                        break;
+                    case Menu.MENU_MAIN_VIEW:
+                        break;
+                    case Menu.MENU_MAIN_ADD:
+                        break;
+                    case Menu.MENU_MAIN_DEL:
+                        break;
+                    case Menu.MENU_MAIN_UPDATE:
+                        break;
+                    case Menu.MENU_MAIN_EXIT:
+                        Environment.Exit(0);
+                        break;
+                }
+            }
         }
     }
 }
--- 20210409/View/Menu.cs
+++ 20210409/View/Menu.cs
@@ -8,19 +8,32 @@ namespace _20210409.View
 {
     class Menu
     {
+        public const int MENU_MAIN_RAND = 1;
+        public const int MENU_MAIN_DEL_ALL = 2;
+        public const int MENU_MAIN_VIEW = 3;
+        public const int MENU_MAIN_ADD = 4;
+        public const int MENU_MAIN_DEL = 5;
+        public const int MENU_MAIN_UPDATE = 6;
+        public const int MENU_MAIN_EXIT = 7;
+
+
         public int mainMenu()
         {
+
             Console.WriteLine("----------------------");
             Console.WriteLine("호갱 차량 관리 앱 v1.0");
             Console.WriteLine("----------------------");
             Console.WriteLine("1. 랜덤데이터 생성");
-            Console.WriteLine("2. 데이터 보기");
-            Console.WriteLine("3. 데이터 추가");
-            Console.WriteLine("4. 데이터 삭제");
-            Console.WriteLine("5. 데이터 수정");
-            Console.WriteLine("6. 종료");
+            Console.WriteLine("2. 모든 데이터 삭제");
+            Console.WriteLine("3. 데이터 보기");
+            Console.WriteLine("4. 데이터 추가");
+            Console.WriteLine("5. 데이터 삭제");
+            Console.WriteLine("6. 데이터 수정");
+            Console.WriteLine("7. 종료");
             Console.WriteLine("----------------------");
-            return Convert.ToInt32(Console.ReadLine());
+
+            int num = Convert.ToInt32(Console.ReadLine());
+            return num;
         }
     }
 }
