4e86913de91e57672579a9b74cb9d4c4f8336fe9
dltmdwlr <dltmdwlr@gmail.com>
2021-04-09 11:53:27 +0900

    d

---
+++ 20210409/Main/Program.cs
@@ -0,0 +1,22 @@
+﻿using _20210409.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20210409
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Car().ToString());
+            new Customer("고길동", 100, "010-1111-1111", "서울 턱별시", '남').CustomerInfo();
+            Console.WriteLine();
+
+            Console.WriteLine("영업소 : " + Seller.CENTER);
+            new Seller("김준석", 28, "사장").sellerinfo();
+        }
+    }
+}
---
+++ 20210409/View/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20210409.View
+{
+    class Menu
+    {
+        public int mainMenu()
+        {
+            Console.WriteLine("----------------------");
+            Console.WriteLine("호갱 차량 관리 앱 v1.0");
+            Console.WriteLine("----------------------");
+            Console.WriteLine("1. 랜덤데이터 생성");
+            Console.WriteLine("2. 데이터 보기");
+            Console.WriteLine("3. 데이터 추가");
+            Console.WriteLine("4. 데이터 삭제");
+            Console.WriteLine("5. 데이터 수정");
+            Console.WriteLine("6. 종료");
+            Console.WriteLine("----------------------");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
