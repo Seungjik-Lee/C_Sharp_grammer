c146802e0672f84f0ee8b7b00377b0edc9122d2c
dltmdwlr <dltmdwlr@gmail.com>
2021-04-19 09:28:47 +0900

    커밋

---
+++ 20210415/CustCar0415/CustCar0415/UI/DealView.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace CustCar0415.UI
+{
+    partial class DealView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(39, 104);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(720, 97);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // DealView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Name = "DealView";
+            this.Text = "DealView";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+    }
+}
---
+++ 20210415/CustCar0415/CustCar0415/UI/DealView.cs
@@ -0,0 +1,71 @@
+﻿using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustCar0415.UI
+{
+    public partial class DealView : MaterialForm
+    {
+        public DealView()
+        {
+            InitializeComponent();
+        }
+        private void initDealSmListView()
+        {
+            //string[] data = { "1", "그랜저", "4천만원", "홍길동", "전우치", "2001년4월16일", "4천만원" };
+            //dealSmListView.Items.Add(new ListViewItem(data));
+            for (int i = 1; i < 50; i++)
+            {
+                dealSmListView.Items.Add(new ListViewItem(
+                new string[]
+                {
+                    (i).ToString(),"쏘나타","4천만원", "홍길동", "전우치", "2001년4월16일", "3천만원"
+
+                }
+                ));
+            }
+            setRowColor(Color.White, Color.LightGray);
+            int index = dealSmListView.Items.Count - 1;
+            dealSmListView.Items[index].Selected = true;
+            dealSmListView.Items[index].Focused = true;
+            dealSmListView.EnsureVisible(index);
+
+        }
+
+        private void DealView_Load(object sender, EventArgs e)
+        {
+            initDealSmListView();
+        }
+
+        private void dealSmExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void setRowColor(Color color1, Color color2)
+        {
+            foreach (ListViewItem item in dealSmListView.Items)
+            {
+                if ((item.Index % 2) == 0)
+                {
+                    item.BackColor = color1;
+                }
+                else
+                {
+                    item.BackColor = color2;
+                }
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
